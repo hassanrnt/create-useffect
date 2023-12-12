2c1b5ce4f7ba8f7cfa467592c445260417dbf222
hassanrnt <hassanritouny97@gmail.com>
2023-12-12 15:26:26 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import './data.js'
+import Meme from './Meme.jsx'
+function App() {
+
+  return(
+    <>
+        <Meme/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Meme.jsx
@@ -0,0 +1,46 @@
+import { useState,useEffect } from "react"
+import data from "./data"
+const Meme = () =>{
+    const [myImages,setMyImages] = useState([])
+
+    useEffect(()=>{
+     return   window.scrollTo(
+      { left: 0, top: document.body.scrollHeight, behavior: "smooth" }
+        )
+    },[myImages])
+    const showImage = (id) =>{
+       let chosenImage =  data.find((item) => item.id === id)
+        // console.log(self.crypto.randomUUID(),)
+        setMyImages((prev)=>(
+                [
+                    ...prev,
+                    {
+                        id:self.crypto.randomUUID(),
+                        img:chosenImage.img
+                    }
+                ]
+
+        ))
+    }
+    const deletImage = (id)=>{
+
+            setMyImages( myImages.filter((item)=> item.id !== id))
+    }
+    return (
+        <>
+
+                 {
+                  myImages.map(({id,img})=>(
+                 <img onClick={()=>deletImage(id)} key={id} src={img} />
+                ))
+                }
+
+            <main>
+                <button onClick={()=>showImage(1)}>Insane</button>
+                <button onClick={()=>showImage(2)}>Volume</button>
+                <button onClick={()=>showImage(3)}>Rock</button>
+            </main>
+        </>
+    )
+}
+export default Meme
